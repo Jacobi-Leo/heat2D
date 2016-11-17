a5b54b5eb5d441cf7673d343bfeb0d50fdc468e3
Jacobi-Leo <liuzeyu271828@gmail.com>
2016-11-18 00:01:15 +0800

    Whole framework finished.

--- heat2D.c
+++ heat2D.c
@@ -10,27 +10,64 @@
 #define CY 0.000002
 
 int main(int *argc, char **argv) {
-    int size, size1, size2, tag, p, my_rank;
-    double u_global[X][Y];
-    
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &p);
+	int size, size1, size2, tag, p, my_rank;
+	double u_global[X][Y];
 
-    size1 = X / p;
-    size2 = X -(p-1)*size1;
-    if ( my_rank != n-1 )
-	size = size1;
-    else
-	size = size2;
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    double u_local[2][size+2][Y];
-    int z = 0;
-    tag = 0;
-    if ( my_rank == 0 ) {
-	inidat( X, Y, u_global );
-	memcpy( &u_local[z][0][0], &u_global[0][0], sizeof(double)*size*Y );
-	for ( int i=1; i<p; i++ ) {
+	size1 = X / p;
+	size2 = X -(p-1)*size1;
+	if ( my_rank != n-1 )
+		size = size1;
+	else
+		size = size2;
+
+	double u_local[size+2][Y];
+	double tmp[size+2][Y];
+	tag = 0;
+	if ( my_rank == 0 ) {
+		inidat( X, Y, u_global );
+		memcpy( &u_local[0][0], &u_global[0][0], sizeof(double)*size*Y );
+		for ( int i=1; i<p; i++ ) {
+			if ( i != p-1 )
+				MPI_Send( u_global+i*size1*Y, size1*Y, MPI_DOUBLE, i, tag, MPI_COMM_WORLD );
+			else
+				MPI_Send( u_global+i*size1*Y, size2*Y, MPI_DOUBLE, i, tag, MPI_COMM_WORLD );
+		}
 	}
-    }
+	else
+		MPI_Recv( u_local+Y, size*Y, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD );
+
+	for ( tag=1; tag<ITER+1; tag++ ) {
+		for ( int i=0; i<p; i++ ) {
+			if ( i == 0 ) {
+				//blabla
+			}
+			else if ( i == p-1 ) {
+				//blabla
+			}
+			else {
+				//blabla
+			}
+		}
+	}
+	
+	if ( my_rank == 0 ) {
+		memcpy( &u_global[0][0], &u_local[0][0], sizeof(double)*size*Y );
+		for ( int i=1; i<p; i++ ) {
+			if ( i != p-1 )
+				MPI_Recv( u_global+i*size1*Y, size1*Y, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status );
+			else
+				MPI_Recv( u_global+i*size1*Y, size2*Y, MPI_DOUBLE, i, tag, MPI_COMM_WORLD, &status );
+		}
+		prtdat(X, Y, u_global);
+	}
+	else
+		MPI_Send( u_local+Y, size*Y, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD );
+	
+	MPI_Finalize();
+	return 0;
 }
